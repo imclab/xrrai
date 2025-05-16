de1519aec4cf497832e8095538e3846d1753dc07
imclab <jtunick@theimclab.com>
2025-05-16 15:42:31 -0400

    latest changes (WIP)

--- demos/browser/simple-server.js
+++ demos/browser/simple-server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 // Enable CORS
 app.use(cors());
@@ -12,8 +12,28 @@ app.use(cors());
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
-// Default route serves the viewer
+// Default route serves the fixed XRAI viewer
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'xrai-fixed-viewer.html'));
+});
+
+// Simple viewer route
+app.get('/simple', (req, res) => {
+  res.sendFile(path.join(__dirname, 'xrai-simple-viewer.html'));
+});
+
+// Minimal viewer route
+app.get('/minimal', (req, res) => {
+  res.sendFile(path.join(__dirname, 'minimal-viewer.html'));
+});
+
+// Original viewer route
+app.get('/original', (req, res) => {
+  res.sendFile(path.join(__dirname, 'xrai-original-fixed.html'));
+});
+
+// Broken original viewer route
+app.get('/original-broken', (req, res) => {
   res.sendFile(path.join(__dirname, 'xrai-viewer.html'));
 });
 
